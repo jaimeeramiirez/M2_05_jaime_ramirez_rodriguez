3f4952d0a97b3306d08ec5ae3fb5d9b106e59dc1
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-14 09:29:23 +0000

    ejercicio 1

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import os
+#EJERCICIO1
+num1= int(input("Introduce el primer número: "))
+num2= int(input("Introduce el segundo número: "))
+num3= int(input("Introduce el tercer número: "))
+while True:
+  if num1==0:
+    print("Prueba de nuevo, el primer número no puede ser igual a 0")
+  elif num3>num2>num1:
+    print("Los números estan en orden ascendente")
+  else:
+    print("Los número están en orden descendente")
+  break
 
-print("Hello world from ...")
-os.system("python --version")
 
