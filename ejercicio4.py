1b8a420568795c48fd9133a89ff32e78a9d1d587
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-10-15 11:49:24 +0000

    "caracteres" en ej 4

---
+++ ejercicio4.py
@@ -0,0 +1,15 @@
+#EJERCICIO4.- 
+deportes = ["Baloncesto", "Futbol", "Tenis", "Golf", "Natación"]
+print(deportes)
+
+print("\n")
+
+print(deportes[0],"-->",len(deportes[0]),"caracteres")
+print(deportes[1],"-->", len(deportes[1]), "caracteres")
+print(deportes[2],"-->", len(deportes[2]), "caracteres")
+print(deportes[3],"-->", len(deportes[3]), "caracteres")
+print(deportes[4],"-->", len(deportes[4]), "caracteres")
+print("\n")
+
+print(f"El elemento con más caracteres es: ", deportes[0])
+print("\n")
--- main.py
+++ main.py
@@ -59,17 +59,17 @@ print(f"En el texto aparecen {contador_letra} letras a.")
 print("\n")
 
 
-#EJERCICIO4
+#EJERCICIO4.- 
 deportes = ["Baloncesto", "Futbol", "Tenis", "Golf", "Natación"]
 print(deportes)
 
 print("\n")
 
-print(deportes[0],"-->",len(deportes[0]))
-print(deportes[1],"-->", len(deportes[1]))
-print(deportes[2],"-->", len(deportes[2]))
-print(deportes[3],"-->", len(deportes[3]))
-print(deportes[4],"-->", len(deportes[4]))
+print(deportes[0],"-->",len(deportes[0]),"caracteres")
+print(deportes[1],"-->", len(deportes[1]), "caracteres")
+print(deportes[2],"-->", len(deportes[2]), "caracteres")
+print(deportes[3],"-->", len(deportes[3]), "caracteres")
+print(deportes[4],"-->", len(deportes[4]), "caracteres")
 print("\n")
 
 print(f"El elemento con más caracteres es: ", deportes[0])
